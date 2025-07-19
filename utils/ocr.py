4a6f58da7eb1e5cb6610b30ae1ccb6745ae1e0a7
artintaleei90 <artintaleei@gmail.com>
2025-07-19 08:59:25 +0330

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,10 +1,10 @@
 import requests
 
-API_KEY = "helloworld"  # اگه API واقعی داری جایگزین کن
+API_KEY = "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"  # اگه API واقعی داری جایگزین کن
 
 def extract_text_from_image(image_url):
     payload = {
-        'apikey': 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,
+        'apikey': "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",
         'url': image_url,
         'language': 'fas'
     }
